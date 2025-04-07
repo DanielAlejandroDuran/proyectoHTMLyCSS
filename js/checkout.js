04c487e65ab0620d9ca7c6d88174bf2795927bd6
DanielAlejandroDuran <166954580+DanielAlejandroDuran@users.noreply.github.com>
2025-04-07 12:34:58 -0500

    Haciendo algunos cambios minimos

--- js/checkout.js
+++ js/checkout.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const minusButtons = document.querySelectorAll('.quantity-btn:first-child');
     const plusButtons = document.querySelectorAll('.quantity-btn:last-child');
     
+    
     minusButtons.forEach(button => {
         button.addEventListener('click', function() {
             const quantityDiv = this.nextElementSibling;
@@ -19,4 +20,30 @@ document.addEventListener('DOMContentLoaded', function() {
             quantityDiv.textContent = quantity + 1;
         });
     });
+    
+    document.getElementById("pay-button").addEventListener("click", function () {
+        Swal.fire({
+          title: "Confirm?",
+          text: "Would you like to confirm your purchase?",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, pay now!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            Swal.fire({
+              title: "Payment Complete!",
+              text: "Thank you for your purchase.",
+              icon: "success",
+              timer: 2000,
+              showConfirmButton: false
+            });
+            setTimeout(() => {
+              window.location.href = "checkout.html";
+            }, 2000);
+          }
+        });
+      });
+    
 });
